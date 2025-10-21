a30190dd16d0840cd59e8a72ab71c73881481f5b
LugrenLaxx <lugrenl@gmail.com>
2025-10-22 00:31:53 +0300

    prt2

--- Collections/FlatMap/src/Task.kt
+++ Collections/FlatMap/src/Task.kt
@@ -1,7 +1,6 @@
 // Return all products the given customer has ordered
-fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+fun Customer.getOrderedProducts(): List<Product> = orders.flatMap(Order::products)
 
 // Return all products that were ordered by at least one customer
 fun Shop.getOrderedProducts(): Set<Product> =
-        TODO()
+        customers.flatMap(Customer::getOrderedProducts).toSet()
--- Collections/Fold and reduce/src/Task.kt
+++ Collections/Fold and reduce/src/Task.kt
@@ -1,5 +1,4 @@
 // Return the set of products that were ordered by all customers
-fun Shop.getProductsOrderedByAll(): Set<Product> =TODO()
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers.fold(customers.first().getOrderedProducts()) { acc, customer -> acc.intersect(customer.getOrderedProducts()) }
 
-fun Customer.getOrderedProducts(): Set<Product> =
-    TODO()
+fun Customer.getOrderedProducts(): Set<Product> = orders.flatMap(Order::products).toSet()
